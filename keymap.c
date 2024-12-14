4950aae430ead28681cacf0045cb302cf6d40d6b
Yushi Cui <realYushi@gmail.com>
2024-12-14 15:04:50 +1300

    update

--- keymap.c
+++ keymap.c
@@ -2,8 +2,14 @@
 #include "keychron_common.h"
 
 enum layers { MAC_BASE, WIN_BASE, MAC_FN1, WIN_FN1, FN2, CAPSLOCK, SYMBOLS, NUMBERS };
-enum TD_keycodes { TD_CBR, TD_ABR, TD_PRN, TD_SBR, TD_HYPR };
-tap_dance_action_t tap_dance_actions[] = {[TD_CBR] = ACTION_TAP_DANCE_DOUBLE(KC_LCBR, KC_RCBR), [TD_ABR] = ACTION_TAP_DANCE_DOUBLE(KC_LABK, KC_RABK), [TD_PRN] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN), [TD_SBR] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC), [TD_HYPR] = ACTION_TAP_DANCE_DOUBLE(LALT(KC_BSPC), KC_F19)};
+enum TD_keycodes { TD_CBR, TD_ABR, TD_PRN, TD_SBR };
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_CBR] = ACTION_TAP_DANCE_DOUBLE(KC_LCBR, KC_RCBR),
+    [TD_ABR] = ACTION_TAP_DANCE_DOUBLE(KC_LABK, KC_RABK),
+    [TD_PRN] = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
+    [TD_SBR] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
+};
 // Left-hand home row mods
 #define HOME_A LCTL_T(KC_A)
 #define HOME_S LALT_T(KC_S)
@@ -22,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS,          MO(MAC_FN1),
         LT(CAPSLOCK,KC_ESC),HOME_A,HOME_S,HOME_D,HOME_F,KC_G,             KC_H,    HOME_J,  HOME_K,  HOME_L,   HOME_SCLN,KC_QUOT,  KC_ENT,           MO(FN2),
         KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,     KC_B, KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LOPTN, KC_LCMMD,          LT(SYMBOLS,KC_SPC),LT(NUMBERS,KC_ENT) , HYPR_T(TD_HYPR),LT(SYMBOLS,KC_BSPC),KC_RCMMD,   KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LOPTN, KC_LCMMD,          LT(SYMBOLS,KC_SPC),LT(NUMBERS,KC_ENT), HYPR_T(KC_F19),LT(SYMBOLS,KC_BSPC),KC_RCMMD,   KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_ansi(
         KC_ESC,  KC_1,	   KC_2,	 KC_3,	  KC_4,    KC_5,	KC_6,	  KC_7,    KC_8,	KC_9,	 KC_0,	   KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
