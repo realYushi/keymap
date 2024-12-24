f28f72da526d1fa95af4edd10e3e9907d28bb6ed
Yushi Cui <realYushi@gmail.com>
2024-12-24 18:11:11 +1300

    feat(config.h, keymap.c, rules.mk): add mouse key functionality to enhance user experience with mouse control chore(config.h): define mouse key speed and acceleration settings for better responsiveness chore(keymap.c): add mouse layer to keymap for improved accessibility of mouse functions chore(rules.mk): enable mouse key feature in rules to activate new functionality

--- keymap.c
+++ keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
 
-enum layers { MAC_BASE, WIN_BASE, MAC_FN1, WIN_FN1, FN2, CAPSLOCK, SYMBOLS, NUMBERS };
+enum layers { MAC_BASE, WIN_BASE, MAC_FN1, WIN_FN1, FN2, CAPSLOCK, SYMBOLS, NUMBERS, MOUSE };
 enum TD_keycodes { TD_CBR, TD_ABR, TD_PRN, TD_SBR };
 
 tap_dance_action_t tap_dance_actions[] = {
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS,  KC_1,	   KC_2,	 KC_3,	  KC_4,    KC_5,	KC_6,	  KC_7,    KC_8,	KC_9,	 KC_0,	   KC_MINS,  KC_EQL,   KC_BSPC,          KC_MPLY,
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS,          MO(MAC_FN1),
         LT(CAPSLOCK,KC_ESC),HOME_A,HOME_S,HOME_D,HOME_F,KC_G,             KC_H,    HOME_J,  HOME_K,  HOME_L,   HOME_SCLN,KC_QUOT,  KC_ENT,           MO(FN2),
-        KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,     KC_B, KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,
+        KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	LT(MOUSE,KC_B),     KC_B, KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,
         KC_LCTL, KC_LOPTN, KC_LCMMD,          LT(SYMBOLS,KC_SPC),LT(NUMBERS,KC_ENT), HYPR_T(KC_F19),LT(SYMBOLS,KC_BSPC),KC_RCMMD,   KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_ansi(
@@ -74,10 +74,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______),
     [NUMBERS] = LAYOUT_69_ansi(
         _______, _______,  _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          _______,
-        _______, _______,  _______,  _______, _______, _______, _______,  KC_KP_7, KC_KP_8, KC_KP_9,  _______, _______,  _______,  _______,          _______,
-        _______,KC_F5, LSFT(KC_F11),  KC_F11,  KC_F10, _______,            KC_DOT, KC_KP_4, KC_KP_5, KC_KP_6, _______,   _______,  _______,          _______,
-        _______,           _______,  _______, _______, _______, _______,  _______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3,   _______,  _______,  _______,
-        _______, _______,  _______,       LCTL(KC_F5),          _______,  _______,          _______,           _______,            _______, _______, _______)
+        _______, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          _______,
+        _______,KC_KP_1,   KC_KP_2,  KC_KP_3, KC_KP_1, KC_KP_5,           KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9,  KC_KP_0,  _______,  _______,          _______,
+        _______,           _______,  _______, _______, _______, _______,  _______, _______, _______, _______, _______,   _______,  _______,  _______,
+        _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______),
+    [MOUSE] = LAYOUT_69_ansi(
+        _______, _______,  _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          _______,
+        _______, _______,  _______,  _______, _______, _______, _______,  KC_MS_WH_DOWN, KC_MS_WH_UP, _______,_______,  _______,  _______,  _______,          _______,
+        _______,_______,   _______,  _______, _______, _______,           KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP , KC_MS_RIGHT,  _______,  _______,  _______,          _______,
+        _______,           _______,  _______, _______, _______, _______,  _______, _______, _______, _______, _______,   _______,  _______,  _______,
+        _______, _______,  _______,           _______,          _______,  KC_MS_BTN1,KC_MS_BTN2,           _______,            _______, _______, _______)
 
 };
 #if defined(ENCODER_MAP_ENABLE)
@@ -89,7 +95,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[FN2]	   = {ENCODER_CCW_CW(_______, _______)},
         [CAPSLOCK] = {ENCODER_CCW_CW(KC_MPRV,KC_MNXT)},
         [SYMBOLS]  = {ENCODER_CCW_CW(_______, _______)},
-        [NUMBERS]  = {ENCODER_CCW_CW(_______, _______)}
+        [NUMBERS]  = {ENCODER_CCW_CW(_______, _______)},
+        [MOUSE]    = {ENCODER_CCW_CW(_______, _______)}
 
 	};
 #endif // ENCODER_MAP_ENABLE
