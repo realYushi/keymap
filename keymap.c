4b95d04243f7450d33d55b0a8eb1dce1241709f1
Yushi Cui <realYushi@gmail.com>
2025-07-11 15:38:47 +1200

    feat(keymap.c, config.h, rules.mk): enhance mouse functionality and add new layers
    - Introduced a dedicated mouse layer in keymap.c for improved mouse control.
    - Added mouse key settings in config.h to enable kinetic mode and configure speed and acceleration.
    - Enabled mouse key feature in rules.mk to activate the new functionality.

--- keymap.c
+++ keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
 
-enum layers { MAC_BASE, WIN_BASE, MAC_FN1, WIN_FN1, FN2, SYMBOLS, NUMBERS };
+enum layers { MAC_BASE, WIN_BASE, MAC_FN1, WIN_FN1, FN2, SYMBOLS, NUMBERS, NAV, MOUSE};
 enum TD_keycodes { TD_CBR, TD_ABR, TD_PRN, TD_SBR };
 
 tap_dance_action_t tap_dance_actions[] = {
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS,          MO(MAC_FN1),
         HYPR_T(KC_ESC),HOME_A,HOME_S,HOME_D,  HOME_F,  KC_G,              KC_H,    HOME_J,  HOME_K,  HOME_L,   LT(SYMBOLS,KC_SCLN),KC_QUOT,  KC_ENT, MO(FN2),
         KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,     KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LOPTN, KC_LCMMD,          KC_SPC,           HYPR_T(KC_F19), MO(NUMBERS),KC_BSPC,           KC_RCMMD,          KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LOPTN, KC_LCMMD,          KC_SPC,           LT(MOUSE,KC_F19), KC_BTN2,KC_BTN1,           KC_RCMMD,          KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_ansi(
         KC_ESC,  KC_1,	   KC_2,	 KC_3,	  KC_4,    KC_5,	KC_6,	  KC_7,    KC_8,	KC_9,	 KC_0,	   KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
@@ -71,6 +71,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,           _______,  _______, _______, _______, _______,  _______, _______, _______, _______, _______,   _______,  _______,  _______,
         _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______),
 
+    [MOUSE] = LAYOUT_69_ansi(
+        _______, _______,  _______,  _______, _______, _______, _______,  _______, _______, _______,  _______,  _______,  _______,  _______,          _______,
+        _______, _______,  _______,  _______, _______, _______, _______,  KC_WH_U, KC_WH_D, _______,  _______,  _______,  _______,  _______,          _______,
+        _______, _______,  _______,  _______, _______, _______,           KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  _______,  _______,  _______,          _______,
+        _______,           _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______,  _______,
+        _______, _______,  _______,           _______,          _______,  KC_BTN2,          KC_BTN1,           _______,            _______, _______, _______),
+
+    [NAV] = LAYOUT_69_ansi(
+        _______, _______,  _______,  _______, _______, _______, _______,  _______, _______, _______,  _______,  _______,  _______,  _______,          _______,
+        _______, _______,  _______,  _______, _______, _______, _______,  KC_WH_U, KC_WH_D, _______,  _______,  _______,  _______,  _______,          _______,
+        _______, _______,  _______,  _______, _______, _______,           KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,  _______,  _______,          _______,
+        _______,           _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______,  _______,
+        _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______),
+
 };
 #if defined(ENCODER_MAP_ENABLE)
 	const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
@@ -81,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[FN2]	   = {ENCODER_CCW_CW(_______, _______)},
         [SYMBOLS]  = {ENCODER_CCW_CW(_______, _______)},
         [NUMBERS]  = {ENCODER_CCW_CW(_______, _______)},
-
+        [MOUSE]    = {ENCODER_CCW_CW(_______, _______)},  // Scroll wheel for mouse layer
 	};
 #endif // ENCODER_MAP_ENABLE
 // clang-format on
