b2054d668844279b448a08ee3325489407768043
Yushi Cui <realyushi@gmail.com>
2025-09-11 17:38:23 +1200

    refactor(keymap.c): update home row modifiers for improved layout consistency

--- keymap.c
+++ keymap.c
@@ -11,23 +11,23 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_SBR] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
 };
 // Left-hand home row mods
-#define HOME_Z LCTL_T(KC_Z)
-#define HOME_X LALT_T(KC_X)
-#define HOME_C LGUI_T(KC_C)
-#define HOME_V LSFT_T(KC_V)
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
 // Right-hand home row mods
-#define HOME_M RSFT_T(KC_M)
-#define HOME_COMM RGUI_T(KC_COMM)
-#define HOME_DOT ALT_T(KC_DOT)
-#define HOME_SLSH RCTL_T(KC_SLSH)
+#define HOME_J LSFT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L RALT_T(KC_L)
+#define HOME_SCLN RCTL_T(KC_SCLN)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_69_ansi(
         KC_CAPS,  KC_1,	   KC_2,	 KC_3,	  KC_4,    KC_5,	KC_6,	  KC_7,    KC_8,	KC_9,	 KC_0,	   KC_MINS,  KC_EQL,   KC_BSPC,          KC_MPLY,
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS,          MO(MAC_FN1),
-        LT(HYPR,KC_ESC),KC_A,KC_S,	 KC_D,	  KC_F,    KC_G,              KC_H,    KC_J,	KC_K,	 KC_L,   LT(SYMBOLS,KC_SCLN),KC_QUOT,  KC_ENT,   MO(FN2),
-        KC_LSFT,           HOME_Z,	 HOME_X,  HOME_C,  HOME_V,	KC_B,     KC_B,    KC_N,	HOME_M,	 HOME_COMM,  HOME_DOT,	 HOME_SLSH,  KC_RSFT, KC_UP,
+        LT(HYPR,KC_ESC),   HOME_A,HOME_S,HOME_D,HOME_F,KC_G,              KC_H,  HOME_J,  HOME_K,   HOME_L,    LT(SYMBOLS,KC_SCLN),KC_QUOT,  KC_ENT, MO(FN2),
+        KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,     KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,
         KC_LCTL, KC_LOPTN, KC_LCMMD,          KC_SPC,           LT(MOUSE,KC_F19), LT(NUMBERS,KC_F18),KC_BSPC,           KC_RCMMD,          KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_ansi(
