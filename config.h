e840dcc08a25599b56515520f6b84e6e4aded6a6
Yushi Cui <realyushi@gmail.com>
2026-01-08 22:48:52 +1300

    refactor(keymap): simplify layers and tap config

--- config.h
+++ config.h
@@ -1,11 +1,10 @@
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 300
+#define TAPPING_TERM 250
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM 100
 #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
 // Allow holding keys while tapping other keys
-#define PERMISSIVE_HOLD
 #define CHORDAL_HOLD
 #define FLOW_TAP_TERM 150
 // mouse
